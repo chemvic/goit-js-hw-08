7e18270f1224038923084a5facc589ab655183b4
chemvic <chemvic79@gmail.com>
2023-02-14 15:33:42 +0200

    убрал с списке картинок стили

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,8 +2,6 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// console.log(galleryItems);
-
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
