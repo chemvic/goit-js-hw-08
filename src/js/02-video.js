946f061b94fd67587b131d08aef42d48aa6e1349
chemvic <chemvic79@gmail.com>
2023-02-17 20:02:53 +0200

    3 задача

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -18,22 +18,3 @@ localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
  localStorage.setItem("videoplayer-current-time", "0");
 });
 }
-
-
-
-
-
-
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+// const currentDataForm = '';
+const formData = {};
+const STORAGE_KEY = "feedback-form-state";
+const formEl = document.querySelector('.feedback-form');
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
+
+//    console.log(savedData);
+// throttle(onInput,500)
+showCurrentData();
+
+formEl.addEventListener('input', onInput);
+
+function onInput(event) {
+    const {email,message} = event.currentTarget.elements;    
+    const data = {
+        email: email.value,
+        message: message.value
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+   
+    
+}
+ 
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    // event.currentTarget.reset();
+   
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    console.log(savedData);
+    localStorage.removeItem(STORAGE_KEY);  
+    inputEl.value = "";
+   textareaEl.value = "";
+}
+
+function showCurrentData() {
+    if (savedData!==null) {
+         inputEl.value = savedData.email||"";
+   textareaEl.value = savedData.message||"";
+    }
+}
