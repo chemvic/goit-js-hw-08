ac0f3aa67185fafca5c98fc3d83f509d4d89ea32
chemvic <chemvic79@gmail.com>
2023-02-17 23:57:58 +0200

    ДОБАВИЛ ПРОВЕРКУ НА НАЛИЧИЕ проигранного времени перед  установкой его из локали

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,19 +2,21 @@
 import Player from '@vimeo/player';
 import throttle  from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
-
+const CURRENT_TIME = "videoplayer-current-time";
 const player = new Player(iframe);
+if (localStorage.getItem(CURRENT_TIME)) {
+     player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
 
- player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+}
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay() {     
     player.getCurrentTime().then(function(seconds) {
     // seconds = the current playback position
-localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
 
 }).catch(function(error) {
- localStorage.setItem("videoplayer-current-time", "0");
+ localStorage.setItem(CURRENT_TIME, "0");
 });
 }
